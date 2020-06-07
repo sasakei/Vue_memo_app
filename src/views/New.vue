302aa09c2e6147d97914c172a4ccad42fa722cd5
sasaki kei <sasakei1021@icloud.com>
2020-06-07 16:51:54 +0900

    first-commit

---
+++ src/views/New.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="editor">
+        <Header> New Memo </Header>
+        <textarea name="memo" v-model="memoBody"></textarea>
+        <button @click="save">保存</button>
+    </div>
+</template>
+
+
+
+<script>
+import Header from "@/components/header.vue"
+export default {
+    name: "new",
+    components: {
+        Header
+    },
+    data: function() {
+        return {
+            memoBody: ""
+        }
+    },
+    methods: {
+        save: function() {
+            this.$store.commit("save", {
+                body: this.memoBody
+            })
+            this.$router.push("/");
+        }
+    }
+}
+</script>
